6f1ee79ed97bca76a0a4cd0a755a9a61ed2b173d
Dylan Bustos Artavia <dylansba777@gmail.com>
2026-03-18 16:57:25 -0600

    6.5

--- app/models/tire.py
+++ app/models/tire.py
@@ -16,6 +16,7 @@ class Tire(db.Model):
     size = db.Column(db.String(20), nullable=True)
     notes = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.utcnow)
+    status = db.Column(db.String(30), nullable=False, default="EN_TALLER_BODEGA")
 
     def __repr__(self) -> str:
         return f"<Tire {self.tire_number}>"
